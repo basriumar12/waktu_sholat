384edf566247eba358b9e3e3d175a72adb364b62
basri umar <basriumar12@gmail.com>
2020-01-21 09:26:36 +0700

    Initial commit

---
+++ lib/logo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Logo {
+  Logo._();
+
+  static const _kFontFam = 'Logo';
+
+  static const IconData cloud_moon = const IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData cloud_sun = const IconData(0xe809, fontFamily: _kFontFam);
+  static const IconData sun = const IconData(0xe80a, fontFamily: _kFontFam);
+  static const IconData moon = const IconData(0xe80b, fontFamily: _kFontFam);
+  static const IconData cloud = const IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData moon_1 = const IconData(0xe811, fontFamily: _kFontFam);
+  static const IconData sunrise = const IconData(0xe815, fontFamily: _kFontFam);
+}
